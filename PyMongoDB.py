e062aeb769de32c294241bed130a5811b742e3e7
Tony <g00372842@gmit.ie>
2022-03-15 17:52:01 +0000

    Updating dummy values for PyMongoDB.py part 2

--- PyMongoDB.py
+++ PyMongoDB.py
@@ -4,7 +4,7 @@ client = pymongo.MongoClient("mongodb://localhost:27017/")
 myDB = client["SmartTabs"]
 myInfo = myDB["MouseTracker"]
 
-multiDict = [{'App': 'pycharm64', 'Duration': 4}, {'App': 'Chrome', 'Duration': 3}, {'App': 'Spotify', 'Duration': 0.4}, {'App': 'File Navigation', 'Duration': 0.7}, {'App': 'Discord', 'Duration': 0.5}, {'App': 'Steam', 'Duration': 1}]  # dummy values to check if connection works with MongoDB
+multiDict = [{'x1': 'pycharm64', 'y1': 4, 'x2': 'Chrome', 'y2': 3, 'x3': 'Spotify', 'y3': 0.4, 'x4': 'File Navigation', 'y4': 0.7, 'x5': 'Discord', 'y5': 0.5, 'x6': 'Steam', 'y6': 1, 'x7': 'Apex Legends', 'y7': 6}]  # dummy values to check if connection works with MongoDB
 myInfo.insert_many(multiDict)
 print("Done")
 
